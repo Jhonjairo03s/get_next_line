62d8139619fb05489acfebe3bf280235b3c2d3f7
jhon valencia <jhonjairo.1331@gmail.com>
2025-01-20 22:38:58 +0100

    se agregaron las librerias <fcntl.h> y <stdio.h>

--- get_next_line.h
+++ get_next_line.h
@@ -13,6 +13,9 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+#  include <fcntl.h> // necesario para `open`
+# include <stdio.h> // necesario para `printf`
+
 char	*get_next_line(int fd);
 
 #endif
